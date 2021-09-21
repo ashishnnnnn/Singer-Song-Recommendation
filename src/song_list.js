7f82f7fdc52bf9a0a71c1c901c7edc6c73eddc14
ASHISH KUMAR <40332678+ashishnnnnn@users.noreply.github.com>
2021-09-21 21:50:49 +0530

    Adding movie name

--- src/song_list.js
+++ src/song_list.js
@@ -1,21 +1,21 @@
 const songs = {
   Mohit: [
-    ["Saiyaara", "4.5"],
-    ["Tum Se Hi", "3.5"],
-    ["Tum Ho", "4.7"],
-    ["Matarghasti", "4.0"],
+    ["Saiyaara", "4.5", "Ek Tha Tiger"],
+    ["Tum Se Hi", "3.5", "Jab We Met"],
+    ["Tum Ho", "4.7", "Rockstar"],
+    ["Matarghasti", "4.0", "Tamasha"]
   ],
   Arijit: [
-    ["Tum Hi Ho", "4.9"],
-    ["Jo Bheji Thi Duwa", "4.2"],
-    ["Aai Dil Hai Mushqil", "3.2"],
+    ["Tum Hi Ho", "4.9", "Aashique 2"],
+    ["Jo Bheji Thi Duwa", "4.2", "Shanghai"],
+    ["Aai Dil Hai Mushqil", "3.2", "Aai Dil Hai Mushqil"]
   ],
   Udit: [
-    ["Cheez Badi", "3.1"],
-    ["Ae Mere Humsafar", "4.2"],
-    ["Jo Bhi Kasmein", "4.5"],
-    ["Tip Tip Barsa Pani", "4.7"],
-  ],
+    ["Cheez Badi", "3.1", "Mohra"],
+    ["Ae Mere Humsafar", "4.2", "Qayamat Se Qayamat Tak"],
+    ["Jo Bhi Kasmein", "4.5", "Raaz"],
+    ["Tip Tip Barsa Pani", "4.7", "Mohra"]
+  ]
 };
 
 export { songs };
